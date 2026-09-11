2cde487b28ab47da186e73db6ccf5a2d9b82b8eb
Harshit <imharshit10@gmail.com>
2026-09-11 16:15:44 +0530

    Add files via upload

--- Day 5/Question5.py
+++ Day 5/Question5.py
@@ -27,8 +27,29 @@ def create_bank_account(owner_name, initial_balance: float) -> dict:
     def get_statement():
         return owner_name, balance, deepcopy(history)
 
-    return dict(deposit = ) 
+    return dict(deposit = deposit, withdraw = withdraw, statement = get_statement)
+ 
 def main():
-    ...
+    # Initial State
+    print(AUDIT_TRANSACTION_COUNT) # Output: 0
+
+    # Create account
+    acc = create_bank_account("Arham", 1000.0)
+    acc["withdraw"](2000.0)
+    # Deposit
+    acc["deposit"](200.0)
+
+    # Withdraw
+    acc["withdraw"](150.0)
+
+    # Get statement
+    owner, bal, txn_history = acc["statement"]()
+    print(owner)       # Output: Arham
+    print(bal)         # Output: 1050.0
+    print(txn_history) # Output: ['Account created with 1000.0', 'deposit 200.0', 'withdraw 150.0']
+
+    # Verify global log count
+    print(AUDIT_TRANSACTION_COUNT) # Output: 2
+
 if(__name__ == "__main__"):
     main()
